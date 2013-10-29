d89e0f99700c49c243d1c36a435c95f74ed7bcb1
Leask Huang <i@leaskh.com>
2013-10-30 00:52:12 +0800

    adding temporary version of flora_pac url request feature, 2nd

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-$proxy = @$_GET['proxy'];
+$proxy = @$_GET['proxy'] ?: 'SOCKS5 127.0.0.1:8964; SOCKS 127.0.0.1:8964';
 
 $file = file_get_contents('flora_pac.pac');
 
-if ($proxy) {
-    $file = preg_replace('/{{PROXY}}/', $proxy, $file);
-}
+$file = preg_replace('/{{PROXY}}/', $proxy, $file);
 
 echo $file;
