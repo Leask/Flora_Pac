e91d8fdf1f4887d277b514721560f7be9d564b15
Leask Huang <i@leaskh.com>
2013-10-30 21:51:50 +0800

    adding application/x-ns-proxy-autoconfig to header for pac host, 3rd

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $file = file_get_contents('flora_pac.pac');
 
 $file = preg_replace('/{{PROXY}}/', $proxy, $file);
 
-header('Content-Type: application/x-ns-proxy-autoconfig');
+header('Content-Type: application/x-ns-proxy-autoconfig; charset=UTF-8');
+header('Content-Disposition: attachment; filename="flora_pac.pac"');
 
 echo $file;
